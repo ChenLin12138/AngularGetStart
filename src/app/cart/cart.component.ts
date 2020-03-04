edac5a860f39b51be798d9a03204f1c79f5bcfd8
ChenLin12138 <scorpion_chenlin@163.com>
2020-03-04 15:51:47 +0800

    表单

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
-
+//引入表单
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -10,11 +11,26 @@ import { CartService } from '../cart.service';
 export class CartComponent implements OnInit {
 
   items;
+  checkoutForm;
 
-  constructor(private cartService : CartService) { }
+  //通过构造函数注入组件
+  constructor(private cartService : CartService, private formBuilder : FormBuilder) { 
+    //通过FormBuilder.group()构建一个表单模型
+    this.checkoutForm = this.formBuilder.group({
+      name : '',
+      address : ''
+    });
+  }
 
   ngOnInit(): void {
     this.items = this.cartService.getItems();
+  }
+
+  onSubmit(customerData){
+    console.warn('Your order has been submitted', customerData);
+    //结账后清空购物车和表单。
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
   }
 
 }
